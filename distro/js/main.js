835b31426d469634d10f756dee4884834ecee8d7
Rodrigo Muniz <rodrigo@mamangava.com>
2018-02-20 15:01:53 -0300

    ajuste nos scroll fix e movel

--- distro/js/main.js
+++ distro/js/main.js
@@ -1,5 +1,33 @@
 $(function () {
     $('.lazy').Lazy();
+
+
+    $(window).scroll(function () {
+
+        var pxScrollado = $(window).scrollTop();
+        var inicioFix = 1000;
+        var fimFix = 2100;
+
+        console.log("pxScrollado", pxScrollado);
+
+        //After scrolling 100px from the top...
+        if (pxScrollado <= inicioFix) {
+            $('#venda-mais').css('background-attachment', 'scroll');
+            $('#sua-lista').css('background-attachment', 'scroll');
+            $('#formidaveis').css('background-attachment', 'scroll');
+
+        } else if ((pxScrollado >= inicioFix) && (pxScrollado <= fimFix)) {
+            $('#venda-mais').css('background-attachment', 'fixed');
+            $('#sua-lista').css('background-attachment', 'fixed');
+            $('#formidaveis').css('background-attachment', 'fixed');
+
+        } else {
+            $('#formidaveis').css('background-attachment', 'scroll');
+        }
+
+    });
+
+
 });
 
 //http://jquery.eisbehr.de/lazy/
